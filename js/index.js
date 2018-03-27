5ce6cb2a942366d3c6f649608a253015d0c30576
Fabio Duque <duqueregis@gmail.com>
2018-03-27 13:45:33 +1100

    First commit simple webApp that fetchs quotes and display.

---
+++ js/index.js
@@ -0,0 +1,47 @@
+var idMsg = 0;
+var actualQuote = {};
+var nextQuote = {};
+var previousQuote = {};
+
+
+function getQuote() {
+	//feeding previous card quote
+	$(".titlePrevious").html(truncateString(previousQuote.title,25));
+   $(".textPrevious").html(truncateString(previousQuote.body,60));
+   $(".idPrevious").html("quote#: " +previousQuote.id);
+   //feeding actual card quote
+	$(".title").html(truncateString(actualQuote.title,25));
+   $(".text").html(truncateString(actualQuote.body,60));
+   $(".id").html("quote # : " + actualQuote.id);
+	previousQuote = actualQuote;
+   //feeding next card quote
+		var root = 'https://jsonplaceholder.typicode.com';
+		idMsg += 1;
+		$.ajax({
+		  url: root + '/posts/' + idMsg,
+		  method: 'GET'
+		}).then(function(data) {
+		  	nextQuote = data;
+		  $(".titleNext").html(truncateString(nextQuote.title,25));
+		   $(".textNext").html(truncateString(nextQuote.body,60));
+		   $(".idNext").html("quote#: " + nextQuote.id);
+		   actualQuote = nextQuote;
+});
+}
+
+function truncateString(str, num) {
+// Clear out that junk in your trunk
+var strAnswer ='';
+if(str !== undefined) {
+	console.log(str);
+	if (str.length > num) {
+	    strAnswer = str.slice(0,num-3);
+	    strAnswer += '...';
+	  } 
+	return strAnswer;
+	}	
+}
+
+$(document).ready(function () {
+  $("#getMessage").on("click", getQuote);
+});
